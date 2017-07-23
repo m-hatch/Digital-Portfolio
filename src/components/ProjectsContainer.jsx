fd6162268838ce0e89ee0765130039a8ae7675fa
m-hatch <montehatch@gmail.com>
2017-07-23 17:08:41 -0400

    show vertical btn when project in view

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -5,12 +5,45 @@ import Section from './Section';
 export default React.createClass({
 
   getProjects: function() {
-    const myData = this.props.projects;
-    const projects = myData.map((project, index) => {
+    const projects = this.props.projects.map((project, index) => {
       return <Project project={ project } key={ project.name } />;
     });
     
     return projects;
+  },
+
+  getScrollTop: function() {
+    return document.documentElement.scrollTop || document.body.scrollTop;
+  },
+
+  getScrollCenter: function() {
+    return this.getScrollTop() + (window.innerHeight/2);
+  },
+
+  getOffsetTop: function(element) {
+    return this.getScrollTop() + element.getBoundingClientRect().top;
+  },
+
+  handleScroll: function(event) {
+    const self = this;
+    const scrollCenter = this.getScrollCenter();
+    const projects = document.querySelectorAll('.project');
+
+    Array.prototype.forEach.call(projects, function(el){
+      const elemPos = self.getOffsetTop(el);
+
+      // add active class when project in view
+      ((elemPos < scrollCenter) && (elemPos + el.clientHeight > scrollCenter)) 
+        ? el.classList.add('project--active') : el.classList.remove('project--active');
+    });
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener('scroll', this.handleScroll);
   },
 
   render: function() {
