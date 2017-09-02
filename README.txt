d64b7d372e3dcd113ba68856b982b6f388560c40
m-hatch <montehatch@gmail.com>
2017-09-02 14:42:10 -0400

    add Contact content

--- README.txt
+++ README.txt
@@ -13,10 +13,15 @@ npm install --save-dev extract-text-webpack-plugin
 #npm install --save-dev image-webpack-loader
 npm install --save-dev file-loader
 npm install --save-dev url-loader
+npm install --save redux
+npm install --save react-redux
 
 ## Run the Development Server
 webpack-dev-server
 #node_modules/.bin/webpack-dev-server
+
+## Run production build
+webpack -p
 
 ## Run tests
 npm run test
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,8 +7,17 @@ export default React.createClass({
     return (
       <Section heading="Contact" bg="white">
 
-          <div className="contact" style={{height: '200px'}}>
+          <div className="contact">
+
+            <h3 className="contact__heading">Want to work together?</h3>
             
+            <p>I am currently accepting new offers and would love to hear about yours. 
+              Please take a moment to tell me about it.</p>
+
+            <button className="button contact__button">
+              Send a message
+            </button>
+
           </div>
 
       </Section>
