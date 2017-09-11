4f16c30b9298bebb3e58e1bd2a2db124cfcaf4e1
m-hatch <montehatch@gmail.com>
2017-09-11 04:00:01 -0400

    add typewriter effect

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -5,22 +5,73 @@ import * as actions from '../actions/actions';
 
 export const Splash = React.createClass({
 
+  getTypeText: function() {
+    const text = this.props.text.position_title;
+
+    const textHtml = text.split('').map(function(char){
+      let html;
+
+      if (char === ' ') { 
+        html = '<span class="type">&nbsp;</span>';
+      } else if (char === '/') { 
+        html = '<span class="type__break type"></span>';
+      } else { 
+        html = '<span class="type">'+char+'</span>';
+      }
+
+      return html;
+    });
+
+    return textHtml.join('').toString() + '<span class="typewriter__cursor"></span>';
+  },
+
+  typewriter: function() {
+    const spanList = document.getElementsByClassName('type');
+    
+    if (this.isInView()) {
+      this.clearType(spanList);
+
+      for (var i = 0; i < spanList.length; i++) {
+        (function(i) {
+          setTimeout(function() { 
+            spanList[i].classList.add('type--active'); 
+          }, 100 * i);
+        })(i);
+      }
+    }
+  },
+
   parallax: function(event) {
     const scrollTop = getScrollTop();
     const elementHeight = this.refs.splash.clientHeight;
 
-    // only call when element is in view
-    if (scrollTop < elementHeight) {
+    if (this.isInView()) {
       this.props.dispatch(actions.parallax(scrollTop * .7));
       this.props.dispatch(actions.setSplashOpacity((elementHeight - scrollTop) / elementHeight));
     }
   },
 
+  clearType: function(spanList) {
+    for (var i = 0; i < spanList.length; i++) {
+      spanList[i].classList.remove('type--active');
+    }
+  },
+
+  isInView: function() {
+    const scrollTop = getScrollTop();
+    const elementHeight = this.refs.splash.clientHeight;
+
+    return (scrollTop < elementHeight) ? true : false;
+  },
+
   componentDidMount: function() {
+    this.typewriter();
+    window.setInterval(this.typewriter, 5000);
     window.addEventListener('scroll', this.parallax);
   },
 
   componentWillUnmount: function() {
+    window.clearInterval(this.typewriter);
     window.removeEventListener('scroll', this.parallax);
   },
 
@@ -42,7 +93,9 @@ export const Splash = React.createClass({
             <p className="splash__portfolio">{ this.props.text.title }</p>
 
             <div className="splash__title">
-              <p className="splash__title__text">{ this.props.text.position_title }</p>
+              <p className="typewriter"
+                dangerouslySetInnerHTML={{ __html: this.getTypeText() }}>
+              </p>
             </div>
 
           </div>
