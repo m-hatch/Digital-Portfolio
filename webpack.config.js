1f31d0a3f691e4420c82210416e41a3ce44f4ad1
m-hatch <hatch@hq.acm.org>
2017-09-21 16:52:41 -0400

    fix loader depricated warning in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          loader: 'css-loader?importLoaders=1',
+          use: 'css-loader?importLoaders=1',
         }),
       },
       {
