ee0389acc5e48be7bdb9ef2c7205515c64ebe598
m-hatch <montehatch@gmail.com>
2017-10-21 20:22:41 -0400

    add image filter animation

--- src/components/ProjectImage.jsx
+++ src/components/ProjectImage.jsx
@@ -5,8 +5,10 @@ export default (props) => {
   return (
     <div className="project__image">
       <a href={ props.link ? props.link : props.github } target="_blank">
-        <div className="image__filter"></div>
-        <img className="project__img" 
+        <div className="image image__filter">
+          <i className="image__icon fa fa-search"></i>
+        </div>
+        <img className="image" 
           src={ 'img/' + props.img } 
           alt={ props.imgAlt } />
       </a>
