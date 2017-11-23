d8bcfa777e0791aa368c5dca755f67be8fb3e0b7
m-hatch <montehatch@gmail.com>
2017-11-23 15:09:20 -0500

    add nav highlighting for routes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import ModalContainer from './ModalContainer';
 
-export default (props) => {
+class Blog extends React.Component {
 
-  return (
-    <div> 
+  constructor(props) {
+    super(props);
+    this.checkRoute = this.checkRoute.bind(this);
+  }
 
-      <h1>Blog</h1>
+  componentDidMount() {
+    this.checkRoute(this.props.location.pathname);
+  }
 
-      <ModalContainer />
+  checkRoute(path) {
+    const navLinks = document.querySelectorAll('#topNav a');
 
-    </div>
-  );
+    Array.prototype.forEach.call(navLinks, function(el){
+
+      // add active class to nav link when route matches
+      let route = path.substr(1, path.length);
+
+      if (route.indexOf('/') !== -1) {
+        route = route.substr(0, route.indexOf('/'));
+      }
+
+      (route === el.getAttribute('href')) 
+        ? el.classList.add('topnav__link--active') 
+        : el.classList.remove('topnav__link--active');
+    });
+  }
+
+  render() {
+    return (
+      <div> 
+
+        <h1>Blog</h1>
+
+        <ModalContainer />
+
+      </div>
+    );
+  }
   
 }
+
+export default Blog;
