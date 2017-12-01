e491429b87f99ab76ec105a67e21b76e2594b837
m-hatch <hatch@hq.acm.org>
2017-12-01 15:57:59 -0500

    add sidebar nav list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,14 +7,34 @@ export default (props) => {
   const articles = props.articles || [];
   const sortedList = articleSort(articles);
 
+  // build nested lists
+  const sidebar = sortedList.map((category, index) => {
+    return (
+      <li key={ index }>
+        <span className="sidebar__topic">{ category[0].topic }</span>
+        <ul className="sidebar__list">{
+          category.map((post, i) => {
+            return (
+              <div>
+                <Link key={i} className="blog__link" 
+                  to={ `${props.navPath}/${post.nav_label}` }>
+                  { post.title }
+                </Link>
+              </div>
+            );
+          })
+        }</ul>
+      </li>
+    )
+  });
+
 console.log(sortedList);
   return (
     <div className="blog__col blog__sidebar">
-      <h2>Browse Topics</h2>
+      <h2 className="blog__browse">Browse Topics</h2>
 
-      <ul></ul>
+      <ul className="sidebar">{sidebar}</ul>
 
-      <Link className="blog__link" to={ `${props.navPath}/${articles[0].nav_label}` } >{ articles[0].topic }</Link>
     </div>
   );
 
