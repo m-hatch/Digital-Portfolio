3e105205e2a12b0d594051409e134a8216e3a168
m-hatch <hatch@hq.acm.org>
2018-03-12 13:06:28 -0400

    extract font-awesome icons to reduce resources weight

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -8,7 +8,7 @@ export default (props) => {
       return (
         <div className={ skill.is_empty ? "expertise__box expertise--empty" : "expertise__box" }
           key={ index }>
-          <i className={ "fa expertise__icon " + skill.icon }></i>
+          <i className={ "expertise__icon " + skill.icon }></i>
           <h4 className="expertise__area">{ skill.area }</h4>
           <p>{ skill.description }</p>
         </div>
